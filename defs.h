d45e322d6a2b853be0a3e349e82cb5ce7be7b6fc
dishap22 <dishapant22@gmail.com>
2024-11-14 14:36:49 +0530

    initialize storage server

--- defs.h
+++ defs.h
@@ -1,4 +1,7 @@
 /* LIMITS */
 #define MAX_CLIENTS 100
 #define MAX_STORAGE_SERVERS 100
-#define MAX_NAME_SIZE 1024
+#define MAX_NAME_SIZE 1024 // max length for name of client/server
+#define MAX_PATH_SIZE 1024 // max length for path of file
+#define MAX_FILE_SIZE 1024 // max length for file content
+#define MAX_PATHS 100 // max number of paths in a storage server
--- storage_server.c
+++ storage_server.c
@@ -1,1 +1,39 @@
 #include "headers.h"
+
+int ssSocket;
+
+int main(char argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+    ssSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ssSocket < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
+
+    struct sockaddr_in ssAddr, clientAddr, nmAddr;
+    memset(&nmAddr, 0, sizeof(nmAddr));
+    memset(&ssAddr, 0, sizeof(ssAddr));
+    memset(&clientAddr, 0, sizeof(clientAddr));
+    ssAddr.sin_family = AF_INET;
+    ssAddr.sin_addr.s_addr = INADDR_ANY;
+    ssAddr.sin_port = htons(port);
+
+    if (bind(ssSocket, (struct sockaddr *) &ssAddr, sizeof(ssAddr)) < 0) {
+        perror("Error binding socket");
+        close(ssSocket);
+        exit(1);
+    }
+    printf("Storage Server started on port %d\n", port);
+
+    if (close(ssSocket) < 0) {
+        perror("Error closing socket");
+        exit(1);
+    }
+
+    return 0;
+}
