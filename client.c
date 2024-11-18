a73d3a8ae500f88d3443890b985eec3ffc38e276
Shruthi150 <shruthibathini2006@gmail.com>
2024-11-18 09:40:37 +0530

    added some initialisation code

--- client.c
+++ client.c
@@ -1,1 +1,55 @@
-#include "headers.h"
+#include "client.h"
+
+void error_exit(const char *message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+int initialize_client() {
+    int sock;
+    struct sockaddr_in naming_server_addr;
+
+    // Create the socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        error_exit("Socket creation failed");
+    }
+
+    // Set up the server address struct
+    memset(&naming_server_addr, 0, sizeof(naming_server_addr));
+    naming_server_addr.sin_family = AF_INET;
+    naming_server_addr.sin_port = htons(NAMING_SERVER_PORT);
+
+    if (inet_pton(AF_INET, NAMING_SERVER_IP, &naming_server_addr.sin_addr) <= 0) {
+        close(sock);
+        error_exit("Invalid IP address format");
+    }
+
+    // Connect to the Naming Server
+    if (connect(sock, (struct sockaddr *)&naming_server_addr, sizeof(naming_server_addr)) < 0) {
+        close(sock);
+        error_exit("Connection to Naming Server failed");
+    }
+
+    printf("Connected to Naming Server at %s:%d\n", NAMING_SERVER_IP, NAMING_SERVER_PORT);
+    return sock;
+}
+
+int send_request(int sock, const char *request) {
+    if (send(sock, request, strlen(request), 0) < 0) {
+        perror("Failed to send request");
+        return -1;
+    }
+    printf("Request sent: %s\n", request);
+    return 0;
+}
+
+ssize_t receive_response(int sock, char *buffer, size_t buffer_size) {
+    memset(buffer, 0, buffer_size);
+    ssize_t received_bytes = recv(sock, buffer, buffer_size, 0);
+    if (received_bytes < 0) {
+        perror("Failed to receive response");
+        return -1;
+    }
+    return received_bytes;
+}
--- client.h
+++ client.h
@@ -1,5 +1,20 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
-#endif
+#define NAMING_SERVER_IP "10.2.141.242" // Replace with actual IP of Naming Server
+#define NAMING_SERVER_PORT 5050         // Replace with actual port of Naming Server
+#define BUFFER_SIZE 1024
+
+int initialize_client();
+int send_request(int sock, const char *request);
+ssize_t receive_response(int sock, char *buffer, size_t buffer_size);
+void error_exit(const char *message);
+
+#endif // CLIENTS_H
